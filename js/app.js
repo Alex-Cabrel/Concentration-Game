5c62b583d16b060fa9f95b00eb3ce9abeaad544b
Alex <alexcodes2000.com>
2017-07-23 14:45:01 +0100

    improvements

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@ var images = ["bird1.jpg", "bird2.jpg", "bird3.jpg", "bird4.jpg", "dog1.jpg", "d
 				, "bird1.jpg", "bird2.jpg", "bird3.jpg", "bird4.jpg", "dog1.jpg", "dog2.jpg", "dog3.jpg", "dog4.jpg"];
 var $board = $('.game');
 
-var game_started = false;
-var image_clicked_one = false;
-var image_clicked_two = false;
-var number_of_moves = 0;
-var total_successful_swaps = 0;
+var image_clicked_one;
+var image_clicked_two;
+var number_of_moves;
+var total_successful_swaps;
+var game_started;
+var timer;
+
+
+
+$('.modal').css({"display":"block"});
+
+
+// Used to close modal
+function closeModal() {
+    $('.modal').css({"display":"none"});
+}
 
 function shuffle(array) {
     var currentIndex = array.length
@@ -24,61 +35,90 @@ function shuffle(array) {
 }
 
 function MemoryGame() {
-    var cards = shuffle(images);
+    /* Main Game function, this function gets shuffled images (cards)
+        and look over them to add on the game board.
+        */
+
+    // Initialising variables
+    game_started = false;
+    image_clicked_one = false;
+    image_clicked_two = false;
+    number_of_moves = 0;
+    total_successful_swaps = 0;
+
+    var cards = shuffle(images); // shuffle images
+
+    //empties the gameboard. this is very essential when player refreshes
     $board.empty();
-    //$moveNum.text('0');
+
+    // loop over the images and add them to the game board.
     for (var i = 0; i < cards.length; i++) {
         var image_name = cards[i].split('.');
         var image_id = image_name[0] + '-' + i.toString();
         $board.append($('<span class="img-box" id=' + image_id + '><li class="img-box-front" id=' + image_id + 
         	'><img src="images/' + cards[i] + '"></li> <li class="img-box-back"></li></span>'));
+
+        // Binds onclick event to each card
         addOnclickEvent(image_id);
     }
 
+    // Initializes game timer to zero
     $(".time-played").text('0:0:00');
+
+    //Initialize moves output
+    $(".moves-played").text(number_of_moves);
 };
 
 
 function addOnclickEvent(id) {
+    /* This function binds an the click even to
+        each card to enable swapping
+        */
     $('span#' + id).click(function () {
-        $('#' + id).addClass('switch-side');
-        if (!image_clicked_one) {
-            image_clicked_one = id;
-            if (!game_started) {
-                game_started = true;
-                GameTimer();
-            }
-        } else {
-            image_clicked_two = id;
-            if (image_clicked_two.split('-')[0] == image_clicked_one.split('-')[0]) {
-                $('span#'+image_clicked_one).off('click');
-                $('span#'+image_clicked_two).off('click');
-                $('li#'+image_clicked_one).addClass('img_box-found');
-                $('li#'+image_clicked_two).addClass('img_box-found')
-                total_successful_swaps += 1;
-                image_clicked_one = false;
-                image_clicked_two = false;
-
-                if (total_successful_swaps == images.length / 2) {
-                    alert('game is over');
+        if(!image_clicked_two){
+            $('#' + id).addClass('switch-side');
+            if (!image_clicked_one) {
+                image_clicked_one = id;
+                if (!game_started) {
+                    game_started = true;
+                    GameTimer();
                 }
-            } else {
-                number_of_moves += 1;
-                $(".moves-played").text(number_of_moves);
+            } else{
+                image_clicked_two = id;
+                if (image_clicked_two.split('-')[0] == image_clicked_one.split('-')[0]) {
+                    $('span#'+image_clicked_one).off('click');
+                    $('span#'+image_clicked_two).off('click');
+                    $('li#'+image_clicked_one).addClass('img_box-found');
+                    $('li#'+image_clicked_two).addClass('img_box-found')
+                    total_successful_swaps += 1;
+                    image_clicked_one = false;
+                    image_clicked_two = false;
 
-                if (number_of_moves == 10) {
+                    if (total_successful_swaps == images.length / 2) {
+
+                        modal.style.display = "block";
+                        $('.moves_stats').text(number_of_moves + total_successful_swaps);
+                        $('.time_stats').text(timer);
+                    }
+                } else{
+                    number_of_moves += 1;
+                    setTimeout(function () {
+                        $('span#' + image_clicked_one).removeClass('switch-side');
+                        $('span#' + image_clicked_two).removeClass('switch-side');
+                        image_clicked_one = false;
+                        image_clicked_two = false;
+                    }, 500);
+                }
+
+                // update moves
+                var moves = number_of_moves + total_successful_swaps;
+                $(".moves-played").text(moves);
+                if (moves == 10) {
                     $('.star3').hide();
-                } else if (number_of_moves == 14) {
+                } else if (moves == 14) {
                     $('.star3').hide();
                     $('.star2').hide();
                 }
-
-                setTimeout(function () {
-                    $('span#' + image_clicked_one).removeClass('switch-side');
-                    $('span#' + image_clicked_two).removeClass('switch-side');
-                    image_clicked_one = false;
-                    image_clicked_two = false;
-                }, 800);
             }
         }
     });
